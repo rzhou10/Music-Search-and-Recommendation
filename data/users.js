849fd3601d05bc7a46f87e8ffb1e607e63b7f579
fasoliaa1 <fasoliaa1@montclair.edu>
2018-12-05 20:23:27 -0500

    Added users.js file to data folder

---
+++ data/users.js
@@ -0,0 +1,78 @@
+const mongoCollections = require("../config/mongoCollections");
+const Users = mongoCollections.Users;
+const bcrypt = require("bcrypt");
+const saltRounds = 16;
+const uuid = require("uuid");
+
+const exportedMethods = {
+    createNewUser : async (username, firstName, lastName, email, password) => {
+        try{
+            let UsersCollection = await Users();
+            
+            const newUser = {
+            _id: uuid.v4(),
+            username: username,
+            hashedPassword: bcrypt.hashSync(password, saltRounds),
+            profile:    
+                {firstName: firstName,
+                lastName: lastName,
+                email: email,
+                favories: null,
+                history: null}
+            };
+            
+            let users = await UsersCollection;
+            await users.insertOne(newUser);
+            return await users.findOne({ _id : newUser._id});
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    /*updateUser : async (id, updatedRecipe) => {
+        try {
+            if(typeof(id) !== 'string') {
+                throw TypeError(`${id} is not a valid id!`);
+            }
+            let setSet = {};
+            if(updatedRecipe.title) {
+                if(typeof(updatedRecipe.title) !== 'string') {
+                    throw TypeError(`${updatedRecipe.title} is not a valid title!`);
+                }
+                setSet.title = updatedRecipe.title;
+            } 
+            if(updatedRecipe.ingredients) {
+                if(!Array.isArray(updatedRecipe.ingredients)) {
+                    throw TypeError(`${updatedRecipe.ingredients} is not a valid array of ingredients!`);
+                }
+                setSet.ingredients = updatedRecipe.ingredients;
+            }
+            if(updatedRecipe.steps) {
+                if(!Array.isArray(updatedRecipe.steps)) {
+                    throw TypeError(`${updatedRecipesteps} is not a valid array of steps!`);
+                }
+                setSet.steps = updatedRecipe.steps;
+            }
+            let recipes = await Users();
+            await recipes.findOneAndUpdate({_id: id}, { $set: setSet });
+            return await recipes.findOne({_id : id});
+        } catch(err) {
+            throw err;
+        }
+    },
+
+    deleteUser : async (id) => {
+        try {
+            if(typeof(id) !== 'string') {
+                throw TypeError(`${id} is not a valid id!`);
+            }
+            let recipes = await Users();
+            return await recipes.deleteOne({_id: id});
+        } catch(err) {
+            throw err;
+        }
+    }, 
+    */
+}
+
+module.exports = exportedMethods;
